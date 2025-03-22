26af80d2b7530070c80de5ba5a9274e670133a7c
nlduy0310 <nlduy0310@gmail.com>
2025-03-22 13:31:13 +0700

    configure dev tools

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,3 +1,8 @@
 export default function App() {
-  return <h1 className="">Hello, world!</h1>;
+	return (
+		<>
+			<h1 className="text-red-300">Hello, world!</h1>
+			<button className="btn">Button</button>
+		</>
+	);
 }
